76fc3d8ad5d8ced28df869391bfad56adf65b38f
Amadéo Ponce <aponce@gaming.tech>
2026-03-19 10:11:07 +0100

    Add Gameplay

---
+++ Sources/Editor/RogueGameplay.cpp
@@ -0,0 +1,13 @@
+#include "RogueGameplay.h"
+
+#include <iostream>
+#include <ostream>
+
+void RogueGameplay::Start()
+{
+}
+
+void RogueGameplay::Update(float deltaTime)
+{
+
+}
--- Sources/GameAssembly/Game.h
+++ Sources/GameAssembly/Game.h
@@ -1,8 +1,10 @@
 #pragma once
+#include <memory>
 #include <Termina/Scripting/API/ScriptingAPI.hpp>
 #include "ImGui/imgui.h"
 #include "ImGui/ImGuizmo.h"
 #include "ImGui/imgui_internal.h"
+#include "RogueGameplay.h"
 
 using namespace TerminaScript;
 
@@ -16,6 +18,7 @@ enum GameState {
 class Game : public TerminaScript::ScriptableComponent
 {
 public:
+    std::unique_ptr<RogueGameplay> gameplay;
     GameState gameState = GameCharacterStats;
 
     Game() = default;
